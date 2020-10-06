71ef03516e8ff1986880005c34114f3463a27f4b
Arturo <arturplur@gmail.com>
2020-10-06 00:40:27 -0500

    stitch docs

--- app.py
+++ app.py
@@ -27,19 +27,41 @@ class FacebookAccess:
     def accounts(self):
         me = User(fbid = 'me')
         my_accts = list(me.get_ad_accounts())
-        #print(my_accts) 
-        #print(type(my_accts))
-
+     
         def params(self):
             params = {
+                'filtering': [
+                    {
+                        'field': 'spend',
+                        'operator': 'GREATER_THAN',
+                        'value': '5'
+
+                    }
+                ],
+                #'breakdowns': [
+                #    'publisher_platform',
+                #    'platform_position',
+                #    'device_platform',  
+                #],
+
+                'time_increment': 'monthly',
+                'sort': ['campaign_name_descending'],            
                 'level': 'campaign'
             }
+            return params
         
+        #https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group
+        #https://www.stitchdata.com/docs/integrations/saas/facebook-ads#campaigns
         def fields(self):
             fields = [
+                'campaign_id',
                 'campaign_name',
-                'spend'
+                'objective',
+                'account_id',
+                'updated_time'          
+                
             ]
+            return fields
 
         for acct in range(len(my_accts)):
             #print(my_accts[acct])
@@ -49,13 +71,18 @@ class FacebookAccess:
                for account_id, id in my_accts[acct].items():
                    if 'act_' in id:
                        results.append(id)
-                       #print(result[0])
                        for result in range(len(results)):
                            insights = AdAccount(results[result]).get_insights(
-                               params = params(self),
+                               params =  params(self),
                                fields = fields(self)
-                               )
+                           )
+                           #for insight in insights:
+                           #    print(insight['campaign_name'])
+
                            print(insights)
+                           
+
+                           
             
             
             #spend = my_accts[acct].api_get(fields=[
